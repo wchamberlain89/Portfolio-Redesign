89c88d8577be9455f8b5ef7b4bd7b6c54483d58a
wc <wchamberlain89@gmail.com>
2020-08-26 19:09:19 -0700

    BugFix - Build on Netlify

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Footer.css';
-import SocialIcons from '../SocialIcons';
+
 
 const Footer = () => {
   return (
     <footer className="footer__container">
-      <SocialIcons color="white" className="footer__icons" />
+     
     </footer>
   )
 }
